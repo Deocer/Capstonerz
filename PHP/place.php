4452ede1a0f4d51e4423f5ffd60d2691245dffd7
Deocer Morales <moralesdeocer@gmail.com>
2021-08-12 19:02:18 +0800

    fixed place

--- PHP/place.php
+++ PHP/place.php
@@ -200,5 +200,4 @@ $near = json_decode($MapquestData, TRUE);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
->>>>>>> c4a0a598708132d0d4e9d9cdb5e0fda015cef18a
 </html>
