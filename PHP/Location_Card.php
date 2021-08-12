69517c1723a103a5aac593d81106dab7162eba4c
Deocer Morales <moralesdeocer@gmail.com>
2021-08-12 19:06:07 +0800

    fixes

--- PHP/Location_Card.php
+++ PHP/Location_Card.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include 'CreateDesImgandTxt.php';
 
@@ -30,36 +29,3 @@ class Place_Box{
 
 
  ?>
-=======
-<?php
-include 'CreateDesImgandTxt.php';
-
-
-class Place_Box{
-  private $q;
-  public function __construct($q,$lat,$lot) {
-  $des = new Destination($q);
-  $imgurl = $des->getimg();
-  $desc = $des->getdesc();
-
-  $destination_box = '
-
-  <div class="card " style="width: 300px;">
-    <img src="'.$imgurl.'>" class="card-img-top" alt="NOT AVAILABLE">
-    <div class="card-body mx-auto">
-      <h5 class="card-title text-center p-3">'.str_replace('%20',' ',$q).'</h5>
-      <p class="card-text">'.$desc.'</p>
-      <a href="PHP/place.php?nm='.$q.'&lat='.$lat.'&lot='.$lot.'&desc='.$desc.'" class="btn btn-primary " type="button" >CHECK OUT</a>
-    </div>
-  </div>
-  ';
-
-  echo $destination_box;
-    }
-
-}
-
-
-
- ?>
->>>>>>> c4a0a598708132d0d4e9d9cdb5e0fda015cef18a
--- user.php
+++ user.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include 'PHP/CreateRecList.php';
 
@@ -56,79 +55,10 @@ $lot = '120.9842';
 	</div>
  	
 
-
-
  </section>
 	
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
  </body>
-=======
-<?php
-include 'PHP/CreateRecList.php';
-
-
-$lat = '14.5995';
-$lot = '120.9842';
-
-
-?>
-
-<!DOCTYPE html>
- <html>
- <head>
- 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- 	<meta charset="utf-8">
- 	<title></title>
- </head>
- <body>
- <section class="container-fluid p-0 m-0" height='100px';>
- 	<div class="row">
-
- 		<div class="bg-dark p-5 col-2 text-center">
- 			<h2 class="p-0 m-0 ">WANDER</h2>
- 			<br>
- 			<img src="img.jpg" width="100%" height="150px">
- 			<br>
- 			<br>
- 			<h4 class="mx-auto">Name</h4>
- 			<br>
-		 	 <nav class="nav flex-column">
-			  <a class="nav-link active" href="#">Active</a>
-			  <a class="nav-link" href="#">Link</a>
-			  <a class="nav-link" href="#">Link</a>
-			  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-			</nav>
-		</div>
-
-
- 	
-		<div class="col-7">
-			<h3 class="mb-3 mb-md-0 text-light bg-dark text-center text-sm-start p-3">TRAVEL FEED</h3>
-		 	
-		</div>
-
-
-
-		 <div class="col-3 bg-dark mx-auto">
-		 	<h3 class="mb-3 mb-md-0 text-light bg-dark text-center text-sm-start p-3">Recomended Places</h3>
-		 		<?php 
-				 $des = new RecomendedP($lat,$lot);
-		 		 ?>
-				 </div>
-		 </div>
-
-	</div>
- 	
-
-
-
- </section>
-	
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
- </body>
->>>>>>> c4a0a598708132d0d4e9d9cdb5e0fda015cef18a
  </html>
